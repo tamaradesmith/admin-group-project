b4fcbc7ec11e560b30b7c6ca6c5ac2680bda849d
Logan Kane <logan.kane1@gmail.com>
2019-10-05 11:36:24 -0700

    added sessions controller, sessions to routes, and sessions management to application controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,21 @@
 class UsersController < ApplicationController
   def new
-    @user = User.new
-end
+      @user = User.new
+  end
 
-def create
-    @user = User.new user_params
-    if @user.save
-        session[:user_id] = @user.id
-        redirect_to root_path
+  def create
+      @user = User.new user_params
+      if @user.save
+          session[:user_id] = @user.id
+          redirect_to root_path
+      else
+          render :new
+      end
+  end
 
-    else
-        render :new
-    end
-end
+  private
 
-
-private
-
-def user_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
-end
+  def user_params
+      params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+  end
 end
