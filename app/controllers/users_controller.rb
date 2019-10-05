7fa8b9b54b900229b45e19e96f00e068007ee58e
Logan Kane <logan.kane1@gmail.com>
2019-10-05 14:48:19 -0700

    added csv import function to create user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,9 @@ class UsersController < ApplicationController
   end
 
   def create
+    #this method should allow for authorization and remove it from the view
+    #I'm not entirely sure on the implementation yet, but from cancancan it should hide create a user if not ADMIN
+    #authorize! :manage, User
       @user = User.new user_params
       if @user.save
           session[:user_id] = @user.id
@@ -11,6 +14,12 @@ class UsersController < ApplicationController
       else
           render :new
       end
+  end
+#This will allow for users to be imported and then afterwards redirects
+  def import
+    User.import(params[:file])
+    #After import, redirect and say if it worked or not.
+    redirect_to(root_url, notice: "Imported User file.")
   end
 
   private
