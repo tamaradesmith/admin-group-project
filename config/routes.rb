a318f7b8e50b28bc1f6feaf98bcd4699538928b6
DaseulChun <nandaseul@gmail.com>
2019-10-06 00:37:06 -0700

    add all stutdents list in Course Show Page

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,10 @@ Rails.application.routes.draw do
   resources :sessions, only:[:new, :create, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  resources :courses
+  resources :courses do
+    resources :enrollments, only: [:create, :destroy]
+  end
+  
    root 'welcome#home'
   resources :attendance, only: [:index, :show, :edit, :update]
   resources :users do
