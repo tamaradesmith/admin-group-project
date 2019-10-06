f7e64aa7178288d8689bab967feecd24fbb420c5
Mao Li <anrenyil@gmail.com>
2019-10-05 21:38:05 -0700

    Assign assignments to class. Assign the Instructor to mark assignments of a class

--- app/controllers/course_assignments_controller.rb
+++ app/controllers/course_assignments_controller.rb
@@ -2,9 +2,8 @@ class CourseAssignmentsController < ApplicationController
     before_action :find_course_assignment, only: [ :update, :destroy]
     before_action :find_course
     def index
-        @assignments = Assignment.select('assignments.name, assignments.id, courses.id as course_id').joins("LEFT JOIN course_assignments ON course_assignments.assignment_id = assignments.id LEFT JOIN courses ON course_assignments.course_id = #{@course.id}  ").paginate(page:params[:page],per_page:5)
-
-
+        @assignments = Assignment.select('assignments.name, assignments.id, course_assignments.due_date , course_assignments.user_id, courses.id as course_id, course_assignments.id as relation_ship_id ').joins("LEFT JOIN course_assignments ON course_assignments.assignment_id = assignments.id LEFT JOIN courses ON course_assignments.course_id = courses.id AND courses.id = #{@course.id}").paginate(page:params[:page],per_page:10)
+        @markers = User.all().where("role_id = 2")
     end
     
     def create
@@ -32,7 +31,7 @@ class CourseAssignmentsController < ApplicationController
     private
     
     def course_assignment_params
-        params.require(:course_assignment).permit(:due_date, :assignment_id, :user_id )
+        params.permit(:due_date, :assignment_id, :user_id )
     end
     
     def find_course_assignment
