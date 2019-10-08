c186ddb5390ef69e556591761babe00bcd958ec2
Mao Li <anrenyil@gmail.com>
2019-10-07 20:22:10 -0700

    echarts

--- app/controllers/marks_controller.rb
+++ app/controllers/marks_controller.rb
@@ -6,6 +6,18 @@ class MarksController < ApplicationController
         @course = Course.find(params[:course_id])
         @user = User.find(params[:user_id])
         @marks = CourseAssignment.select('assignments.name as assignment_name, assignments.id as assignment_id, course_assignments.due_date , course_assignments.user_id as marker_id, courses.id as course_id, course_assignments.id as relation_ship_id, marks.id, marks.user_id as student_id, marks.grade ').joins("INNER JOIN assignments ON course_assignments.assignment_id = assignments.id INNER JOIN courses ON course_assignments.course_id = courses.id AND courses.id = #{@course.id} LEFT JOIN marks ON marks.course_assignment_id = course_assignments.id AND marks.user_id = #{params[:user_id]}").paginate(page:params[:page],per_page:5)
+        # byebug
+        @marks_array = @marks.collect(&:grade).to_json.html_safe
+        # @marks_array = []
+        # @marks.each do |mark|
+        #     @marks_array.push(mark.grade)
+        # end
+        @assignments_array = @marks.collect(&:assignment_name).to_json.html_safe
+        # @assignments_array = []
+        # @marks.each do |mark|
+        #     @assignments_array.push(mark.assignment_name)
+        # end
+        # byebug
     end
 
     def create
